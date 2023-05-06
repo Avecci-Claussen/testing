340fc6a6b16a7c9e091eba444dd351a7939d6740
Avecci-Claussen <73264647+Avecci-Claussen@users.noreply.github.com>
2023-05-06 13:33:48 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -3,22 +3,25 @@ import binascii
 import ecdsa
 import random
 import multiprocessing
-import time
-import threading
+from Crypto.Hash import RIPEMD160
 
 def pubkey_to_hash160(public_key_bytes):
     sha256 = hashlib.sha256(public_key_bytes).digest()
-    hash160 = hashlib.new('ripemd160')
-    hash160.update(sha256)
+    hash160 = RIPEMD160.new(sha256)
     return hash160.hexdigest()
 
-def find_private_key(input_tuple, progress_counter):
+def find_private_key(input_tuple):
     start_number, end_number, target_hash160, max_attempts = input_tuple
     curve = ecdsa.SECP256k1
+    searched_values = set()
 
-    for number in range(start_number, end_number + 1):
-        with progress_counter.get_lock():
-            progress_counter.value += 1
+    for _ in range(max_attempts):
+        number = random.randint(start_number, end_number)
+        print(f"Scanning private key (decimal): {number}")
+
+        if number in searched_values:
+            continue
+        searched_values.add(number)
 
         private_key = ecdsa.SigningKey.from_secret_exponent(number, curve)
         public_key = private_key.get_verifying_key().to_string("compressed")
@@ -28,32 +31,13 @@ def find_private_key(input_tuple, progress_counter):
             return number
 
     return None
+def parallel_private_key_search(start_number, end_number, target_hash160, num_cores, max_attempts=1000000):
+    pool = multiprocessing.Pool(processes=num_cores)
+    input_tuples = [(start_number, end_number, target_hash160, max_attempts) for _ in range(num_cores)]
 
-def report_progress(progress_counter):
-    while True:
-        time.sleep(10)
-        with progress_counter.get_lock():
-            keys_scanned = progress_counter.value
-            print(f"{keys_scanned} keys scanned so far.")
-            progress_counter.value = 0
-
-def parallel_private_key_search(start_number, end_number, target_hash160, num_cores):
-    segment_size = (end_number - start_number) // num_cores
-    input_tuples = [(start_number + i * segment_size, start_number + (i + 1) * segment_size, target_hash160, segment_size) for i in range(num_cores)]
-
-    # Fix the end number for the last tuple
-    last_start, _, _, _ = input_tuples[-1]
-    input_tuples[-1] = (last_start, end_number, target_hash160, segment_size)
-
-    progress_counter = multiprocessing.Value('L', 0)
-    progress_thread = threading.Thread(target=report_progress, args=(progress_counter,))
-    progress_thread.start()
-
-    with multiprocessing.Pool(processes=num_cores) as pool:
-        results = [pool.apply_async(find_private_key, args=(t, progress_counter)) for t in input_tuples]
-        results = [res.get() for res in results]
-
-    progress_thread.join()
+    results = pool.map(find_private_key, input_tuples)
+    pool.close()
+    pool.join()
 
     for result in results:
         if result is not None:
@@ -63,11 +47,12 @@ def parallel_private_key_search(start_number, end_number, target_hash160, num_co
 
 if __name__ == "__main__":
     start_number = 2**29
-    end_number = 2**30
+    end_number = 2**30 - 1
     target_hash160 = "d39c4704664e1deb76c9331e637564c257d68a08"
-    num_cores = 78  # Set the number of CPU cores you want to use.
+    max_attempts = 10000000
+    num_cores = 74  # Set the number of CPU cores you want to use.
 
-    found_private_key = parallel_private_key_search(start_number, end_number, target_hash160, num_cores)
+    found_private_key = parallel_private_key_search(start_number, end_number, target_hash160, num_cores, max_attempts)
     if found_private_key:
         print(f"Private key found:{found_private_key}")
         print(f"Private key found:{ hex(found_private_key) [2:]  }")
