70c0bbf3be422a504cbb2ecf896b62f6f6d6c256
Kha'Zix <73264647+Avecci-Claussen@users.noreply.github.com>
2023-08-10 21:56:26 +0200

    Update fc.py

--- fc.py
+++ fc.py
@@ -1,12 +1,31 @@
 import argparse
+import multiprocessing
 
-def find_solutions(index):
+def find_solutions_for_chunk(args):
+    start, end, index = args
     solutions = []
-    for privkey in range(max(2 * index, 2 ** (index - 1)), int(2 ** (index - 0.10)) + 1):
+    for privkey in range(start, end + 1):
         mod_value = 2 ** index - privkey
         if mod_value >= 0 and mod_value < privkey and 2 ** index == privkey + mod_value:
             solutions.append((privkey, mod_value))
     return solutions
+
+def find_solutions(index):
+    num_processes = multiprocessing.cpu_count()  # Number of available CPUs
+    chunk_size = (int(2 ** (index - 0.10)) - max(2 * index, 2 ** (index - 1))) // num_processes
+
+    args_list = [
+        (max(2 * index, 2 ** (index - 1)) + chunk_size * i, 
+         max(2 * index, 2 ** (index - 1)) + chunk_size * (i + 1) - 1,  # Subtracted 1 to make the end exclusive
+         index) 
+        for i in range(num_processes)
+    ]
+
+    with multiprocessing.Pool(num_processes) as pool:
+        all_solutions = pool.map(find_solutions_for_chunk, args_list)
+
+    # Flattening the list of lists
+    return [solution for sublist in all_solutions for solution in sublist]
 
 def main():
     parser = argparse.ArgumentParser(description="Find solutions for the equation 2^index = privkey + mod_value")
